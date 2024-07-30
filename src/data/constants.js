00754fa6e6e0f4d34a052e1bc44fd6da4df69ab7
Ayush Kumar <79322903+ayush8587@users.noreply.github.com>
2024-07-30 19:50:14 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ayush8587",
   resume:
-    "https://drive.google.com/file/d/1SowQO10WPXb_JWVo1-c96QSWmCRavNZp/view?usp=sharing",
+    "https://drive.google.com/file/d/1QtkhBoubQMQQQKDiztJpH6b7rvWHSTv9/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/singhayush16/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/kumar_ayush_.16/",
