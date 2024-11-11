725fb4ace97d1560faa5dcb87a9c95b7ac0bdf67
Ayush Kumar <79322903+ayush8587@users.noreply.github.com>
2024-11-11 18:48:03 +0530

    Update Experience.jsx

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -60,8 +60,8 @@ const Experience = () => {
             marginBottom: "40px",
           }}
         >
-          My work experience as a Data Analyst and working on different
-          companies and projects.
+{/*           My work experience as a Data Analyst and working on different
+          companies and projects. */}
         </Desc>
 
         <VerticalTimeline>
