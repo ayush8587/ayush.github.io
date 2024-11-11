fa6eff78079542e534a038152b83975e2fc96e79
Ayush Kumar <79322903+ayush8587@users.noreply.github.com>
2024-11-11 18:47:37 +0530

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -104,8 +104,8 @@ const Projects = ({ openModal, setOpenModal }) => {
             marginBottom: "40px",
           }}
         >
-          I have worked on a wide range of projects. From web apps to android
-          apps. Here are some of my projects.
+{/*           I have worked on a wide range of projects. From web apps to android
+          apps. Here are some of my projects. */}
         </Desc>
         <ToggleButtonGroup>
           <ToggleButton
